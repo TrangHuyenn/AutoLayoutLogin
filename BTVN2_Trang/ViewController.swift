8266716d1fd72beb812d125c77e6febd59c27649
TrangHuyenn <trangzazi@gmail.com>
2021-07-02 23:40:24 +0700

    auto layout screen

---
+++ BTVN2_Trang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BTVN2_Trang
+//
+//  Created by Trang Huyền on 6/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
